ac8c827b5c50f7c320d4e3603d4f16629206c6a7
Amir Burbea <amir.burbea@gmail.com>
2022-10-23 19:50:19 -0700

    Formatting

--- Vestaboard.Clock/Numbers/One.cs
+++ Vestaboard.Clock/Numbers/One.cs
@@ -9,7 +9,8 @@ internal sealed class One : Number
         output[0][startIndex + 2] =
         output[1][startIndex + 1] = output[1][startIndex + 2] =
         output[2][startIndex] = output[2][startIndex + 2] =
-        output[3][startIndex + 2] = output[4][startIndex + 2] =
+        output[3][startIndex + 2] = 
+        output[4][startIndex + 2] =
         output[5][startIndex] = output[5][startIndex + 1] = output[5][startIndex + 2] = output[5][startIndex + 3] = BoardCharacter.White;
     }
 }
